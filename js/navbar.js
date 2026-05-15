05f8734cd804b5322eb2bb9cde98dd63c09f83ce
amrks <aymarks@salcc.edu.lc>
2026-05-14 20:25:35 -0400

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ document.querySelector(".writeHeader").innerHTML = `
     <header align="center">
         <div id="nav">
             <a href="reminders.php" class="${window.location.pathname.includes('reminders') ? 'active' : ''}">reminders</a>
-            <a href="calendar.php" class="${window.location.pathname.includes('calendar') ? 'active' : ''}">calender</a>
+            <a href="calendar.php" class="${window.location.pathname.includes('calendar') ? 'active' : ''}">calendar</a>
             <a href="index.php" class="${window.location.pathname.includes('index') ? 'active' : ''}">home</a>
             <a href="goals.php" class="${window.location.pathname.includes('goals') ? 'active' : ''}">goals</a>
             <a href="profile.php" class="${window.location.pathname.includes('profile') ? 'active' : ''}">me</a>
